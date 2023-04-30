e1893389eddd5ef47cfacdc2e19e5af724736b27
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-04-30 06:53:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -95,3 +95,51 @@ app.get("/hack/instagram", async (req, res) => {
 
 app.listen(port);
 console.log(`Server running on port ${port}`);
+
+
+
+const express = require("express");
+const cors = require("cors");
+const ytdl = require("ytdl-core");
+const app = express();
+// const fs = require("fs");
+
+const corsOptions = {
+  origin: "https://vivekfy.netlify.app", // change this origin as your like
+  // origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.use(express.static("./static"));
+const port = process.env.PORT || 5000;
+
+app.get("/", (res) => {
+  res.render("index.html");
+});
+
+app.get("/hack", async (req, res) => {
+  const url = req.query.url;
+  console.log(url);
+  const info = await ytdl.getInfo(url);
+  const title = info.videoDetails.title;
+  const thumbnail = info.videoDetails.thumbnails[0].url;
+  let formats = info.formats;
+
+  const audioFormats = ytdl.filterFormats(info.formats, "audioonly");
+  // const format = ytdl.chooseFormat(info.formats, { quality: "136" });
+  formats = formats.filter((format) => format.hasAudio === true);
+
+  res.send({ title, thumbnail, audioFormats, formats });
+});
+
+
+
+// app.get('*', (req, res) => {
+//   res.render('error')
+// })
+
+app.listen(port, () => {
+  console.log("Running ...");
+});
